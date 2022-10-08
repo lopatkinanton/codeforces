565e28fbc9559b12d3679a6f767ae1b3a4a0c617
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-08 23:45:51 +0300

    Add files via upload

--- lopatkin_a_a/hw-05-02.cpp
+++ lopatkin_a_a/hw-05-02.cpp
@@ -1,24 +1,27 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 int main() {
     int a = 0;
     int b = 1;
     double k = 0.05;
     double e = 0.001;
-    std::cout << "x\t|\ts(x)\t|\tf(x)\n";
-    for (double x = a; x <= b; x += k) {
+    std::cout << std::setw(5) << "x" << std::setw(3) << "|" << std::setw(10) << "s(x)" << std::setw(3) << "|" << std::setw(10) << "f(x)\n";
+    for (double x = a; x - b <= e; x += k) {
         double sum = 0;
         double s = e;
+        double p = 1;
         int i = 1;
-        while (s >= e) {
-            s = std::pow(x, i) * sin(i * M_PI_4);
+        while (std::abs(s) >= e && s != 0) {
+            s = p * sin(i * M_PI_4);
             sum += s;
             i += 1;
+            p *= x;
         }
         sum -= s;
         double f = x * sin(M_PI_4) / (1 - 2 * x * cos(M_PI_4));
-        std::cout << x << "\t|\t" << sum << "\t|\t" << f << '\n';
+        std::cout << std::setw(5) << x << std::setw(3) << "|" << std::setw(10) << sum << std::setw(3) << "|" << std::setw(10) << f << '\n';
     }
 }
--- lopatkin_a_a/hw-05-03.cpp
+++ lopatkin_a_a/hw-05-03.cpp
@@ -1,31 +1,27 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
-
-long long factorial(int a) {
-    long long n = 1;
-    for (int i = 1; i <= a; i += 1)
-        n *= i;
-    return n;
-}
+#include <iomanip>
 
 int main() {
     int a = 0;
     int b = 1;
     double k = 0.1;
     double e = 0.0001;
-    std::cout << "x\t|\ts(x)\t|\tf(x)\n";
-    for (double x = a; x <= b; x += k) {
+    std::cout << std::setw(5) << "x" << std::setw(3) << "|" << std::setw(10) << "s(x)" << std::setw(3) << "|" << std::setw(10) << "f(x)\n";
+    for (double x = a; x - b <= e; x += k) {
         double sum = 0;
         double s = e;
         int i = 0;
-        while (s >= e) {
-            s = (double) std::pow(2 * x, i) / factorial(i);
+        int fact = 1;
+        while (std::abs(s) >= e && s != 0) {
+            if (i > 1) fact *= i;
+            s = (double) std::pow(2 * x, i) / fact;
             sum += s;
             i += 1;
         }
         sum -= s;
         double f = std::pow(M_E, 2 * x);
-        std::cout << x << "\t|\t" << sum << "\t|\t" << f << '\n';
+        std::cout << std::setw(5) << x << std::setw(3) << "|" << std::setw(10) << sum << std::setw(3) << "|" << std::setw(10) << f << '\n';
     }
 }
