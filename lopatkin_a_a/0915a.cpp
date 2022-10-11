730c27d3e94b3550e215437a71f350a4a09c5611
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-11 15:47:08 +0300

    Add files via upload

---
+++ lopatkin_a_a/0915a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n = 0; 
+    int k = 0;
+    std::cin >> n >> k;
+    int t = 1e9;
+    for (int i = 0; i < n; i += 1) {
+        int a = 0;
+        std::cin >> a;
+        if (k % a == 0)
+            t = std::min(t, k / a);
+    }
+    std::cout << t;
+    return 0;
+}
