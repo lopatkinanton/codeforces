dbede2bae407dfa4eda52a1706935152211383f2
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-17 17:56:49 +0300

    Add files via upload

---
+++ lopatkin_a_a/0935b.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    std::string s;
+    std::cin >> s;
+    int x = 0;
+    int y = 0;
+    int k = -1;
+    int m = -1;
+    for (int i = 0; i < n; i += 1) {
+        if (s[i] == 'U')
+            y += 1;
+        if (s[i] == 'R')
+            x += 1;
+        if (y > x && k != 0){
+            m += 1;
+            k = 0;
+        }
+        if (y < x && k != 1) {
+            m += 1;
+            k = 1;
+        }
+    }
+    std::cout << m;
+}
