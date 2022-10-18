75b38f40f31a5678112a876ef023a29d1ceaeb2b
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 16:04:11 +0300

    Update 1717b.cpp

--- lopatkin_a_a/1717b.cpp
+++ lopatkin_a_a/1717b.cpp
@@ -1,16 +1,22 @@
-﻿#include <iostream>
+#include <iostream>
 
-int main()
-{
+int main() {
     int t = 0;
     std::cin >> t;
     for (int a = 0; a < t; a += 1) {
-        int n = 0, k = 0, r = 0, c = 0;
+        int n = 0;
+        int k = 0;
+        int r = 0; 
+        int c = 0;
         std::cin >> n >> k >> r >> c;
         for (int i = 0; i < n; i += 1) {
             for (int j = 0; j < n; j += 1) {
-                if ((j + r) % k == (i + c) % k) std::cout << 'X';
-                else std::cout << '.';
+                if ((j + r) % k == (i + c) % k) {
+                    std::cout << 'X';
+                }
+                else {
+                    std::cout << '.';
+                }
             }
             std::cout << '\n';
         }
