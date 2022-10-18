d0048422ba1ec9f414998cbab69ff89e0c62e8be
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 15:58:25 +0300

    Update 1585a.cpp

--- lopatkin_a_a/1585a.cpp
+++ lopatkin_a_a/1585a.cpp
@@ -8,8 +8,9 @@ int main() {
         int n = 0;
         std::cin >> n;
         std::vector <int> a(n);
-        for (int i = 0; i < n; i += 1)
+        for (int i = 0; i < n; i += 1) {
             std::cin >> a[i];
+        }
         int k = 1;
         for (int i = 0; i < n; i += 1) {
             if (a[i] == 0 && i != 0 && a[i - 1] == 0) {
@@ -21,8 +22,9 @@ int main() {
                     k += 1;
                     continue;
                 }
-                if (a[i - 1] == 1)
+                if (a[i - 1] == 1) {
                     k += 5;
+                }
             }
         }
         std::cout << k << '\n';
