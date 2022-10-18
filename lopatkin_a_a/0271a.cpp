4894060f4e08ee1b923998f60881ba344589949d
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 15:02:20 +0300

    Update 0271a.cpp

--- lopatkin_a_a/0271a.cpp
+++ lopatkin_a_a/0271a.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-int main()
-{
+int main() {
     int y = 0;
     std::cin >> y;
-    for (y += 1; y < 10000; y += 1){
-        int a = y / 1000, b = y / 100 % 10, c = y / 10 % 10, d = y % 10;
+    for (y += 1; y < 10000; y += 1) {
+        int a = y / 1000;
+        int b = y / 100 % 10;
+        int c = y / 10 % 10;
+        int d = y % 10;
         bool x = a != b;
         x = x && a != c;
         x = x && a != d;
