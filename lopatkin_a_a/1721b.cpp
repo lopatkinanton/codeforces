b41eb60af042974c6f0f87e5fc49de2a93e0fdb5
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 16:05:14 +0300

    Update 1721b.cpp

--- lopatkin_a_a/1721b.cpp
+++ lopatkin_a_a/1721b.cpp
@@ -1,15 +1,22 @@
-﻿#include <iostream>
+#include <iostream>
 
-int main()
-{
+int main() {
     int t = 0;
     std::cin >> t;
     for (int i = 0; i < t; i += 1) {
-        int n = 0, m = 0, sx = 0, sy = 0, d = 0;
+        int n = 0;
+        int m = 0;
+        int sx = 0;
+        int sy = 0; 
+        int d = 0;
         std::cin >> n >> m >> sx >> sy >> d;
         int minl = std::min(sx - 1, m - sy);
         int minr = std::min(sy - 1, n - sx);
-        if (minl <= d && minr <= d) std::cout << -1 << std::endl;
-        else std::cout << n + m - 2 << std::endl;
+        if (minl <= d && minr <= d) {
+            std::cout << -1 << std::endl;
+        }
+        else {
+            std::cout << n + m - 2 << std::endl;
+        }
     }
 }
