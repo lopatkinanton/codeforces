134dfb9e8bfef46b6e5251b99b2a8fe0df3ff712
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 15:05:33 +0300

    Update 0915a.cpp

--- lopatkin_a_a/0915a.cpp
+++ lopatkin_a_a/0915a.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <vector>
 
 int main() {
     int n = 0; 
     int k = 0;
     std::cin >> n >> k;
-    int t = 1e9;
+    int t = 1.0E9;
     for (int i = 0; i < n; i += 1) {
         int a = 0;
         std::cin >> a;
-        if (k % a == 0)
+        if (k % a == 0) {
             t = std::min(t, k / a);
+        }
     }
     std::cout << t;
     return 0;
