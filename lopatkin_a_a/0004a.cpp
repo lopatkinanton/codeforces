e45472d4e58de9bc4a034697e281dc270616633e
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 14:41:19 +0300

    Update 0004a.cpp

--- lopatkin_a_a/0004a.cpp
+++ lopatkin_a_a/0004a.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
     int w = 0;
-    cin >> w;
-    if (w % 2 == 0 && w >= 4) cout << "YES";
-    else cout << "NO";
+    std::cin >> w;
+    if (w % 2 == 0 && w >= 4) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
 }
