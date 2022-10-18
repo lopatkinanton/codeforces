51e46a0e9b93b2fe29955e2bfde89a0aa2a36b80
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 15:04:32 +0300

    Update 0617a.cpp

--- lopatkin_a_a/0617a.cpp
+++ lopatkin_a_a/0617a.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
-int main()
-{
+int main() {
     int x = 0;
     std::cin >> x;
-    if (x % 5 == 0) std::cout << x / 5;
-    else std::cout << x / 5 + 1;
+    if (x % 5 == 0) {
+        std::cout << x / 5;
+    }
+    else {
+        std::cout << x / 5 + 1;
+    }
 }
