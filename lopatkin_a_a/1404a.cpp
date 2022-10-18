6ea5a38b55bbe1e2bad633fe2c7551ab2e0c5ebf
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 15:53:40 +0300

    Update 1404a.cpp

--- lopatkin_a_a/1404a.cpp
+++ lopatkin_a_a/1404a.cpp
@@ -16,8 +16,12 @@ int main() {
         for (int i = 0; i < n; i += 1) {
             int x = i % k;
             if (r[x] == '?') {
-                if (s[i] == '0') r[x] = '0';
-                if (s[i] == '1') r[x] = '1';
+                if (s[i] == '0') {
+                    r[x] = '0';
+                }
+                if (s[i] == '1') {
+                    r[x] = '1';
+                }
             }
             else {
                 if (r[x] != s[i] && s[i] != '?') {
@@ -29,10 +33,16 @@ int main() {
         int a = 0;
         int b = 0;
         for (int i = 0; i < k; i += 1) {
-            if (r[i] == '0') a += 1;
-            if (r[i] == '1') b += 1;
+            if (r[i] == '0') {
+                a += 1;
+            }
+            if (r[i] == '1') {
+                b += 1;
+            }
         }
-        if (a > k / 2 || b > k / 2) ans = "NO";
+        if (a > k / 2 || b > k / 2) {
+            ans = "NO";
+        }
         std::cout << ans << '\n';
     }
 
