2a7870a775e772bbc7e576d7b658955905f35524
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 14:54:59 +0300

    Update 0137a.cpp

--- lopatkin_a_a/0137a.cpp
+++ lopatkin_a_a/0137a.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
+int main() {
     std::string s;
     std::cin >> s;
     s += ' ';
-    int k = 0, c = 0, p = 0;
-    for (int i = 0; i < s.length(); i += 1){
-        if (s[i] == 'C') c += 1;
-        if (s[i] == 'P') p += 1;
-        if (c > 0 && s[i] == 'P'){
+    int k = 0;
+    int c = 0;
+    int p = 0;
+    for (int i = 0; i < s.length(); i += 1) {
+        if (s[i] == 'C') {
+            c += 1;
+        }
+        if (s[i] == 'P') {
+            p += 1;
+        }
+        if (c > 0 && s[i] == 'P') {
             k += 1;
             c = 0;
         }
-        if (s[i] == 'C' && p > 0){
+        if (s[i] == 'C' && p > 0) {
             k += 1;
             p = 0;
         }
-        if (c == 5 || p == 5){
+        if (c == 5 || p == 5) {
             k += 1;
             c = 0;
             p = 0;
         }
     }
-    if (c > 0 || p > 0) k += 1;
+    if (c > 0 || p > 0) {
+        k += 1;
+    }
     std::cout << k;
 }
