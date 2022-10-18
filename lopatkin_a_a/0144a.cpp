0c18bd723021ab67fe713fbd265a0e5e7e3fb862
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 14:57:33 +0300

    Update 0144a.cpp

--- lopatkin_a_a/0144a.cpp
+++ lopatkin_a_a/0144a.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 
-int main()
-{
-    int n = 0, maxx = 0, minn = 100, x, y;
+int main() {
+    int n = 0;
+    int maxx = 0;
+    int minn = 100;
+    int x = 0;
+    int y = 0;
     std::cin >> n;
-    for (int i = 0; i < n; i += 1){
+    for (int i = 0; i < n; i += 1) {
         int a = 0;
         std::cin >> a;
-        if (a > maxx){
+        if (a > maxx) {
             maxx = a;
             x = i;
         }
-        if (a <= minn){
+        if (a <= minn) {
             minn = a;
             y = i;
         }
     }
-    if (x < y) std::cout << x + n - y - 1;
-    else std::cout << x + n - y - 2;
+    if (x < y) {
+        std::cout << x + n - y - 1;
+    }
+    else {
+        std::cout << x + n - y - 2;
+    }
 }
