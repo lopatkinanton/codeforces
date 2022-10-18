38eb4293b6d138604af8d8ea5019f5a68bfe3dd8
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 14:48:17 +0300

    Update 0071a.cpp

--- lopatkin_a_a/0071a.cpp
+++ lopatkin_a_a/0071a.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-string intToString(int k) {
-    char a = k / 100 + '0', b = k / 10 % 10 + '0', c = k % 10 + '0';
-    string s;
+std::string intToString(int k) {
+    char a = k / 100 + '0';
+    char b = k / 10 % 10 + '0';
+    char c = k % 10 + '0';
+    std::string s;
     if (k >= 100) {
         s.push_back(a);
         s.push_back(b);
@@ -15,24 +15,28 @@ string intToString(int k) {
         s.push_back(b);
         s.push_back(c);
     }
-    else s.push_back(c);
+    else {
+        s.push_back(c);
+    }
     return s;
 }
 
 int main() {
     int n = 0;
-    cin >> n;
+    std::cin >> n;
     for (int i = 0; i < n; i += 1) {
-        string s;
-        cin >> s;
+        std::string s;
+        std::cin >> s;
         int len = s.length();
-        if (len <= 10) cout << s << endl;
+        if (len <= 10) {
+            std::cout << s << std::endl;
+        }
         else {
-            string ans;
+            std::string ans;
             ans.push_back(s[0]);
             ans += intToString(len - 2);
             ans.push_back(s[len - 1]);
-            cout << ans << endl;
+            std::cout << ans << std::endl;
         }
     }
 }
