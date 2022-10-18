3e7b1889a014ad1007368d2f6b33c0f682f7dd81
lopatkinanton <81617455+lopatkinanton@users.noreply.github.com>
2022-10-18 15:03:40 +0300

    Update 0276a.cpp

--- lopatkin_a_a/0276a.cpp
+++ lopatkin_a_a/0276a.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
-int main()
-{
-    int n = 0, k = 0, m = -2e9;
+int main() {
+    int n = 0;
+    int k = 0;
+    int m = -2.0E9;
     std::cin >> n >> k;
-    for (int i = 0; i < n; i += 1){
-        int f, t;
+    for (int i = 0; i < n; i += 1) {
+        int f = 0;
+        int t = 0;
         std::cin >> f >> t;
-        if (t > k) m = std::max(m, f - (t - k));
-        else m = std::max(m, f);
+        if (t > k) {
+            m = std::max(m, f - (t - k));
+        }
+        else {
+            m = std::max(m, f);
+        }
     }
     std::cout << m;
 }
